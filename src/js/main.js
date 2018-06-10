278314df57105702e7cd98e1dbd20365f1380925
trofinata <miss.happ2012@yandex.ru>
2018-06-10 18:12:23 +0300

    Begining

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	var link = $('.menu-link');
+	var link_active = $('.menu-link_active');
+	var menu = $('.menu');
+	var nav_link = $('.menu a')
+
+	link.click(function(){
+		link.toggleClass('menu-link_active');
+		menu.toggleClass('menu_active');
+	});
+	nav_link.click(function(){
+		link.toggleClass('menu-link_active');
+		menu.toggleClass('menu_active');
+	});
+});
